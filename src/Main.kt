8ad358d9ca778cceca8996fad6f589fa79b1282e
Nurssult <nursultansat88@gmail.com>
2026-09-25 21:27:57 +0500

    HW:Task7

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,25 @@
 fun main() {
-    val firstNumber = 10
-    val secondNumber = 5
-    val thirdNumber = 8
+    val firstUserEmailId = "user_one@gmail.com"
 
-    val result = add(firstNumber, secondNumber)
-    val anotherResult = add(firstNumber, thirdNumber)
+    println(displayAlertMessage(emailId = firstUserEmailId))
+    println()
 
-    println("$firstNumber + $secondNumber = $result")
-    println("$firstNumber + $thirdNumber = $anotherResult")
+    val secondUserOperatingSystem = "Windows"
+    val secondUserEmailId = "user_two@gmail.com"
+
+    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
+    println()
+
+    val thirdUserOperatingSystem = "Mac OS"
+    val thirdUserEmailId = "user_three@gmail.com"
+
+    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
+    println()
 }
 
-fun add(a: Int, b: Int): Int {
-    return a + b
+fun displayAlertMessage(
+    operatingSystem: String = "Unknown OS",
+    emailId: String
+): String {
+    return "There's a new sign-in request on $operatingSystem for your Google Account $emailId."
 }
