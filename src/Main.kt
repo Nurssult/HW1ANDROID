7805269fe32c73ed60ae9322bad4afd9ef6c6b8e
Nurssult <nursultansat88@gmail.com>
2026-09-25 21:03:20 +0500

    HW1:Task4

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,9 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    var discountPercentage: Int = 0
-    var offer: String = ""
-    val item = "Google Chromecast"
+    val numberOfAdults = 20
+    val numberOfKids = 30
+    val total = numberOfAdults + numberOfKids
 
-    discountPercentage = 20
-    offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
-
-    println(offer)
+    println("The total party size is: $total")
 }
