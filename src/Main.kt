2982fd2c497bd183f887143921f1194b7d81d58b
Nurssult <nursultansat88@gmail.com>
2026-09-25 21:30:34 +0500

    HW:Task8

--- src/Main.kt
+++ src/Main.kt
@@ -1,25 +1,13 @@
 fun main() {
-    val firstUserEmailId = "user_one@gmail.com"
+    val steps = 4000
+    val caloriesBurned = pedometerStepsToCalories(steps)
 
-    println(displayAlertMessage(emailId = firstUserEmailId))
-    println()
-
-    val secondUserOperatingSystem = "Windows"
-    val secondUserEmailId = "user_two@gmail.com"
-
-    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
-    println()
-
-    val thirdUserOperatingSystem = "Mac OS"
-    val thirdUserEmailId = "user_three@gmail.com"
-
-    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
-    println()
+    println("Walking $steps steps burns $caloriesBurned calories")
 }
 
-fun displayAlertMessage(
-    operatingSystem: String = "Unknown OS",
-    emailId: String
-): String {
-    return "There's a new sign-in request on $operatingSystem for your Google Account $emailId."
+fun pedometerStepsToCalories(numberOfSteps: Int): Double {
+    val caloriesBurnedPerStep = 0.04
+    val totalCaloriesBurned = numberOfSteps * caloriesBurnedPerStep
+
+    return totalCaloriesBurned
 }
