d8d989ad9828eee315261587898c138ba622addf
Nurssult <nursultansat88@gmail.com>
2026-09-25 20:59:47 +0500

    HW1:Task3

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    println("New chat message from a friend");
+    var discountPercentage: Int = 0
+    var offer: String = ""
+    val item = "Google Chromecast"
+
+    discountPercentage = 20
+    offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
+
+    println(offer)
 }
