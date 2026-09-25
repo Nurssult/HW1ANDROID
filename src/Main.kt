fb4293f449bee9c2bfa6130a61c1b89e4a56290c
Nurssult <nursultansat88@gmail.com>
2026-09-25 20:46:45 +0500

    HW1:Task1

---
+++ src/Main.kt
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    println("Use the val keyword when the value doesn't change. ");
+    println("Use the var keyword when the value can change.");
+    println("When you define a function, you define the parameters that can be passed to it. ");
+    println("When you call a function, you pass arguments for the parameters.");
+}
